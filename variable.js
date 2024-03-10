a6ba6d3fcaa04017dad5782986fa0274e787daff
soniausman <160847802+soniausman@users.noreply.github.com>
2024-03-10 22:22:21 +0500

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,8 @@
+"use strict";
+// string //
+let userName = ("sonia");
+console.log(userName);
+const personName = ("usman");
+console.log(personName);
+var goodName = ("baba");
+console.log(goodName);
---
+++ variable.ts
@@ -0,0 +1,11 @@
+ // string //
+  let userName : string = ("sonia");
+  console.log(userName);
+
+  const personName : string = ("usman");
+  console.log(personName);
+
+  var goodName : string = ("baba");
+  console.log(goodName);
+
+  
