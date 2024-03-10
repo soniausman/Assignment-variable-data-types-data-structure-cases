57f5858de2db7d0b2ba22095623e3a8867a72029
soniausman <160847802+soniausman@users.noreply.github.com>
2024-03-10 22:20:48 +0500

    Add files via upload

---
+++ data_structure.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = ["sonia", "usman", "omer", "maimoona"];
+console.log(personName);
+let goodName = ["sonia", 1, 2, 3, true];
+console.log(goodName);
---
+++ data_structure.ts
@@ -0,0 +1,8 @@
+let personName  : string[] = ["sonia","usman","omer","maimoona"];
+console.log(personName);
+
+let goodName : any[]=["sonia", 1,2,3,true];
+console.log(goodName);
+
+
+
