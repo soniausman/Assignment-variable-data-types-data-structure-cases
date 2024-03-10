6c55407a7da6f76c07cc8a6b69fe1a6f636fc77a
soniausman <160847802+soniausman@users.noreply.github.com>
2024-03-10 22:21:35 +0500

    Add files via upload

---
+++ cases.js
@@ -0,0 +1,10 @@
+"use strict";
+// camel case //
+let personName = "sonia";
+console.log(personName);
+// snake case //
+let person_name = "usman";
+console.log(person_name);
+// pascal case //
+let PersonName = "omer";
+console.log(PersonName);
